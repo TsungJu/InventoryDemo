1d33efce36eb653fbc712a841da7867a50bbe2a6
TsungJu <e285171@gmail.com>
2021-05-20 17:26:41 +0800

    1. Add launch.json. 2. Use a template and static files to render a page.

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 import mysql.connector
 import json
 from flask import Flask
+from flask import render_template
+from datetime import datetime
 
+# create an instance of Flask object
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello, Docker!'
+
+@app.route("/hello/")
+@app.route("/hello/<name>")
+def hello_there(name = None):
+    return render_template(
+        "hello_there.html",
+        name = name,
+        date = datetime.now()
+    )
+
+@app.route("/api/data")
+def get_data():
+    return app.send_static_file("data.json")
 
 @app.route('/widgets')
 def get_widgets():
