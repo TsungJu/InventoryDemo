a97100cda65a43c44aa40aa58ea2a2ae182f4ad0
TsungJu <e285171@gmail.com>
2021-05-21 09:58:24 +0800

    Add VScode create multiple templates example.

--- app.py
+++ app.py
@@ -8,8 +8,16 @@ from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
-    return 'Hello, Docker!'
+def home():
+    return render_template("home.html")
+
+@app.route('/about/')
+def about():
+    return render_template("about.html")
+
+@app.route("/contact/")
+def contact():
+    return render_template("contact.html")
 
 @app.route("/hello/")
 @app.route("/hello/<name>")
