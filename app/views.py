b75482c0e75b59c10e353733a89c57d6c2e5cb8d
TsungJu <e285171@gmail.com>
2021-05-27 09:49:17 +0800

    jinja2 pass variable and add show widget page

--- app/views.py
+++ app/views.py
@@ -3,7 +3,8 @@ from datetime import datetime
 from flask import Flask, render_template
 
 from . import app
-
+import mysql.connector
+import json
 
 @app.route("/")
 def home():
@@ -29,3 +30,79 @@ def hello_there(name = None):
 @app.route("/api/data")
 def get_data():
     return app.send_static_file("data.json")
+
+def get_widgets():
+    mydb = mysql.connector.connect(
+        host="mysqldb",
+        user="root",
+        password="p@ssw0rd1",
+        database="inventory"
+    )
+    cursor = mydb.cursor()
+
+    cursor.execute("SELECT * FROM widgets")
+
+    # this will extract row headers
+    row_headers=[x[0] for x in cursor.description]
+
+    results = cursor.fetchall()
+    json_data=[]
+    for result in results:
+        json_data.append(dict(zip(row_headers,result)))
+
+    #json_data.extend(results)
+
+    cursor.close()
+
+    return json_data
+
+@app.route('/widgets')
+def show_widgets():
+    widgets=get_widgets()
+    return render_template("show_widgets.html",widgets=widgets)
+
+@app.route('/initdb')
+def db_init():
+    mydb = mysql.connector.connect(
+        host="mysqldb",
+        user="root",
+        password="p@ssw0rd1"
+    )
+    cursor = mydb.cursor()
+
+    cursor.execute("DROP DATABASE IF EXISTS inventory")
+    cursor.execute("CREATE DATABASE inventory")
+    cursor.close()
+
+    mydb = mysql.connector.connect(
+        host="mysqldb",
+        user="root",
+        password="p@ssw0rd1",
+        database="inventory"
+    )
+    cursor=mydb.cursor()
+
+    cursor.execute("DROP TABLE IF EXISTS widgets")
+    cursor.execute("CREATE TABLE widgets (name VARCHAR(255), description VARCHAR(255))")
+    #Insert multiple rows
+    insert_multiple_rows_sql="INSERT INTO widgets (name,description) VALUES (%s,%s)"
+    val = [
+        ('Peter', 'Lowstreet 4'),
+        ('Amy', 'Apple st 652'),
+        ('Hannah', 'Mountain 21'),
+        ('Michael', 'Valley 345'),
+        ('Sandy', 'Ocean blvd 2'),
+        ('Betty', 'Green Grass 1'),
+        ('Richard', 'Sky st 331'),
+        ('Susan', 'One way 98'),
+        ('Vicky', 'Yellow Garden 2'),
+        ('Ben', 'Park Lane 38'),
+        ('William', 'Central st 954'),
+        ('Chuck', 'Main Road 989'),
+        ('Viola', 'Sideway 1633')
+    ]
+    cursor.executemany(insert_multiple_rows_sql,val)
+    mydb.commit()
+    cursor.close()
+
+    return 'init database'
